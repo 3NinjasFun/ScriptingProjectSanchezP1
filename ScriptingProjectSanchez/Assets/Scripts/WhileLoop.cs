ca1b3743416f3370868e472613cdb50baa2aed22
3NinjasFun <739237@student.dusd.net>
2022-11-01 09:23:38 -0700

    Wrote 2 looping scripts
    scripting project work: Loops

---
+++ ScriptingProjectSanchez/Assets/Scripts/WhileLoop.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WhileLoop : MonoBehaviour
+{
+    int CupsInTheSink = 4;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        while (CupsInTheSink > 0)
+            {
+            Debug.Log("I've washed a cup!");
+            CupsInTheSink--;
+            }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
