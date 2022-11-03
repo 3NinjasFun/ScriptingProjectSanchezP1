24d78ea30fb7c23016b91df2bbe13bc4f4135dc5
3NinjasFun <739237@student.dusd.net>
2022-11-03 08:34:37 -0700

    added 4 scripts
    scripting project

--- ScriptingProjectSanchez/Assets/Scripts/DoWhileLoops.cs
+++ ScriptingProjectSanchez/Assets/Scripts/DoWhileLoops.cs
@@ -7,7 +7,7 @@ public class DoWhileLoops : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        bool shouldContinue = true;
+        bool shouldContinue = false;
 
         do
         {
