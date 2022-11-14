7d51c6937a7c104f3e90181a8c30031bff5c2fa7
3NinjasFun <739237@student.dusd.net>
2022-11-14 09:58:07 -0800

    Decorated
    added trees arena pool housing  and maze.

---
+++ ScriptingProjectSanchez/Assets/Scripts/Awakining.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Awakining : MonoBehaviour
+{
+    void Awake()
+
+    {
+        Debug.Log("Awake called");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Satrt called");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
